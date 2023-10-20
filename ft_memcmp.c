93596739be22d0900a209975d4dad3eaf17d4fad
42 <entrainment@Yann-2.local>
2023-10-20 14:14:54 +0200

    jsp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,21 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yerbs <yerbs@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/20 12:21:44 by yerbs             #+#    #+#             */
-/*   Updated: 2023/10/20 13:14:44 by yerbs            ###   ########.fr       */
+/*   Created: 2023/10/20 13:10:48 by yerbs             #+#    #+#             */
+/*   Updated: 2023/10/20 13:19:51 by yerbs            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
-
-int ft_tolower(int c)
+int ft_memcmp(const void s1, const void s2, size_t n)
 {
-    if (c >= 'a' && c <= 'c')
-        return (c);
-    return (c + 32);
-}
+    unsigned const char		*t1;
+	unsigned const char		*t2;
 
+	t1 = (unsigned const char *)s1;
+	t2 = (unsigned const char *)s2;
+    if (!s1 || !s2)
+        return (0);
+    while (n)
+    {
+        if (*t1 != *t2)
+            return (*t1 - *t2);
+        t1++;
+        t2++;
+        n--;
+    }   
+    return (0);
+}
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,21 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yerbs <yerbs@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/20 12:21:44 by yerbs             #+#    #+#             */
-/*   Updated: 2023/10/20 13:14:44 by yerbs            ###   ########.fr       */
+/*   Created: 2023/10/20 13:24:18 by yerbs             #+#    #+#             */
+/*   Updated: 2023/10/20 13:28:55 by yerbs            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
-int ft_tolower(int c)
+char    *ft_strnstr(const char *big, const char *little, size_t len)
 {
-    if (c >= 'a' && c <= 'c')
-        return (c);
-    return (c + 32);
+    size_t  i;
+    
+    i = 0;
+    if (!*little)
+        return ((char *)big);
+    while (big[i] && (i + ft_strlen(little)) <= len)
+    {
+        if (ft_strncmp(big + i, little, ft_strlen(little)) == 0)
+            return ((char *) &big[i]);
+        i++;
+    }
+    return (NULL)
 }
-
