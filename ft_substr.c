8e0f9b2526c410e420e1861a4e57d15ba7929ab1
42 <entrainment@Yann-2.local>
2023-10-24 07:31:50 +0200

    substr

---
+++ ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerbs <yerbs@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 07:20:57 by yerbs             #+#    #+#             */
+/*   Updated: 2023/10/24 07:20:58 by yerbs            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start,size_t len)
+{
+	char	*s2;
+	int	compteur;
+
+	s2 = ft_calloc(len, (char));
+	if (!s2)
+		return (NULL);
+	compteur = 0;
+	while(compteur < len)
+	{
+		s2[compteur] = s[start];
+		compteur++;
+		start++;
+	}
+	return (s2);
+}
+
