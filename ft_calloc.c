d1806c89330a653a11aa549b9aa9e207a96663ec
Yann Erbs <yerbs@k2r2p7.42mulhouse.fr>
2023-10-27 12:25:39 +0200

    FINITO

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: yerbs <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 12:53:54 by yerbs             #+#    #+#             */
-/*   Updated: 2023/10/27 08:08:28 by yerbs            ###   ########.fr       */
+/*   Updated: 2023/10/27 12:21:21 by yerbs            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*s;
+	size_t	len;
 
-	if (!nmemb || !size)
-		return (malloc(0));
+	len = nmemb * size;
+	if (size && len / size != nmemb)
+		return (NULL);
 	s = malloc(nmemb * size);
 	if (!s)
 		return (NULL);
